e5d8c190fe5f501e315c8ab799733b4dae150173
Yordanos Legesse <127302898+yordil@users.noreply.github.com>
2023-04-05 13:17:02 +0300

    added cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+     
+     vector <int> mark_of_stu {10 , 20 , 30};
+
+     cout<<mark_of_stu[2];
+   
+
+     return 0;
+
+
+}
